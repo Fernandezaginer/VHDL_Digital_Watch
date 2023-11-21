a8a4e3056df1fc11dfe37d40f6143122ffa36628
pablonhernandez <pablo.nhernandez@alumnos.upm.es>
2023-11-21 20:40:37 +0100

    Revert "Implementacion estado cronometro y formato 12/24h"
    This reverts commit 935a4058d340710e38fedd3264eb5a25f5e71988.

--- src/fsm_global.vhd
+++ src/fsm_global.vhd
@@ -1,9 +1,35 @@
--- Trabajo SED 23/24 Grupo 2
--- Maquina de estados
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 21.11.2023 19:01:21
+-- Design Name: 
+-- Module Name: fsm_global - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
 
 
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
 
 entity fsm_global is
   Port (
@@ -19,67 +45,13 @@ entity fsm_global is
 end fsm_global;
 
 architecture Behavioral of fsm_global is
----------------------------------------------------------------------------------------------------------------------------
---DECLARACION DE COMPONENTES
----------------------------------------------------------------------------------------------------------------------------
-    component Cronometro is
-    Port (  clk         :in std_logic;
-            buttons     : in std_logic_vector(3 downto 0);
-            stateActive : in std_logic_vector(8 downto 0);
-            digits_0to3 : out std_logic_vector(15 downto 0);
-            digits_4to7 : out std_logic_vector(15 downto 0);
-            blink_ctrl  : out std_logic_vector(7 downto 0)         
-   );
-end component;
-
-component display_12_24 is
-    generic(
-        MODE_NUM : std_logic_vector(3 downto 0) := "1111"
-        );
-    Port (
-        clk : in std_logic;
-        buttons: in std_logic_vector(3 downto 0);
-        stateActive: in std_logic_vector(8 downto 0);
-        digits_0to3 : out std_logic_vector(15 downto 0);
-        digits_4to7 : out std_logic_vector(15 downto 0);
-        blink_ctrl : out std_logic_vector(7 downto 0);
-        out_mode : out std_logic
-    );
-end component;
-
     type STATES is (S1,S2,S3,S4,S5,S6,S7,S8,S9);
     signal currentState: STATES := S1;
     signal nextState: STATES;
     
     signal stateAct : std_logic_vector (8 downto 0); --Vector de señales de activación componentes
     
-    signal outFormat12_24 : std_logic := '0'; --señal formato de hora 12/24h
-    
 begin
-------------------INSTANCIACION CRONO--------------------------
-    intsCronometro : Cronometro
-		Port map(
-	        clk => clk, 
-	        buttons => buttons,
-	        stateActive => stateAct,
-	        digits_0to3 => digits_0to3,
-	        digits_4to7 => digits_4to7,
-	        blink_ctrl => blink_ctrl
-		);
-------------------INSTANCIACION FORMATO HORA--------------------------		
-    instFormat12_24 : display_12_24
-		generic map(
-			MODE_NUM => "1000"
-			)
-	    Port map (
-            clk => clk,
-	        buttons => buttons,
-	        stateActive => stateAct,
-	        digits_0to3 => digits_0to3,
-	        digits_4to7 => digits_4to7,
-	        blink_ctrl => blink_ctrl,
-	        out_mode => outFormat12_24
-	    );
 --Paso a siguiente estado
     process (clk)
     begin
@@ -145,15 +117,13 @@ begin
             when S4 =>
                 stateAct <= "000100000" ;   
             when S5 =>
-                stateAct <= "000010000" ;
-            --CRONOMETRO    
+                stateAct <= "000010000" ;    
             when S6 =>
                 stateAct <= "000001000" ;   
             when S7 =>
                 stateAct <= "000000100" ;   
             when S8 =>
                 stateAct <= "000000010" ;   
-            --FORMATO DE HORA
             when S9 =>
                 stateAct <= "000000001"; 
         end case;
