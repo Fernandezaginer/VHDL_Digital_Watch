32dfdfe633d8d7b0fc4c7e8a6c89f21f000b8783
pablonhernandez <pablo.nhernandez@alumnos.upm.es>
2023-11-21 18:44:03 +0100

    Preparacion maquina de estados general
    Se modifica cronometro para adaptarla a la futura maquina de estados

--- src/Cronometro.vhd
+++ src/Cronometro.vhd
@@ -9,6 +9,7 @@ use IEEE.numeric_std.all;
 entity Cronometro is
   Port (clk:in std_logic;
         buttons: in std_logic_vector(3 downto 0);
+        stateActive: in std_logic_vector(8 downto 0);
         digits_0to3 : out std_logic_vector(15 downto 0);
         digits_4to7 : out std_logic_vector(15 downto 0);
         blink_ctrl : out std_logic_vector(7 downto 0)         
@@ -50,78 +51,84 @@ begin
     );
     
 --Paso a sig estado
-    process (buttons, clk)
+    process (stateActive, buttons, clk)
     begin
-        if buttons = "0100" then
-            currentState <= S0;
-        elsif clk'event and clk = '1' then
-            currentState <= nextState;
+        if stateActive = "000001000" then
+            if buttons = "0100" then
+                currentState <= S0;
+            elsif clk'event and clk = '1' then
+                currentState <= nextState;
+            end if;
         end if;
     end process;   
     
 --Cambio estado
-    process (buttons, currentState)
+    process (stateActive, buttons, currentState)
     begin
-        nextState <= currentState;
-        case currentState is
-            when S0 =>
-                if buttons = "0001" then
-                    nextState <= S1;
-                end if;
-            when S1 =>
-                if buttons = "0100" then
+        if stateActive = "000001000" then
+            nextState <= currentState;
+            case currentState is
+                when S0 =>
+                    if buttons = "0001" then
+                        nextState <= S1;
+                    end if;
+                when S1 =>
+                    if buttons = "0100" then
+                        nextState <= S0;
+                    elsif buttons = "0010" then
+                        nextState <= S2;
+                    end if;
+                when S2 =>
+                    if buttons = "0100" then
+                        nextState <= S0;
+                    elsif buttons = "0001" then
+                        nextState <= S1;
+                    end if;            
+                when others =>
                     nextState <= S0;
-                elsif buttons = "0010" then
-                    nextState <= S2;
-                end if;
-            when S2 =>
-                if buttons = "0100" then
-                    nextState <= S0;
-                elsif buttons = "0001" then
-                    nextState <= S1;
-                end if;            
-            when others =>
-                nextState <= S0;
-        end case;
+            end case;
+        end if;
     end process;   
 
 --Logica de estados
-    process (currentState, clkSec)
+    process (stateActive, currentState, clkSec)
     begin
-        case currentState is
-            when S0 => --Reset
-                udsSecs<="0000"; decSecs<="0000"; udsMin<="0000"; decMin<="0000";
-            when S1 => --Play
-                if rising_edge (clkSec) then
-                    if udsSecs = "1001" then --Limite udsSecs = 9
-                        udsSecs <= "0000";
-                        if decSecs = "0101" then --Limite decsecs = 5
-                            decSecs <= "0000";
-                            if udsMin = "1001" then --Limite udsMin = 9
-                                udsMin <= "0000";
-                                if decMin = "0101" then --Limite decMin = 5
-                                    decMin <= "0000";
-                                else 
-                                    decMin <=std_logic_vector(to_unsigned(TO_INTEGER(unsigned(decMin)) + 1, decMin'length)); --Suma "10" min
+        if stateActive = "000001000" then
+            case currentState is
+                when S0 => --Reset
+                    udsSecs<="0000"; decSecs<="0000"; udsMin<="0000"; decMin<="0000";
+                when S1 => --Play
+                    if rising_edge (clkSec) then
+                        if udsSecs = "1001" then --Limite udsSecs = 9
+                            udsSecs <= "0000";
+                            if decSecs = "0101" then --Limite decsecs = 5
+                                decSecs <= "0000";
+                                if udsMin = "1001" then --Limite udsMin = 9
+                                    udsMin <= "0000";
+                                    if decMin = "0101" then --Limite decMin = 5
+                                        decMin <= "0000";
+                                    else 
+                                        decMin <=std_logic_vector(to_unsigned(TO_INTEGER(unsigned(decMin)) + 1, decMin'length)); --Suma "10" min
+                                    end if;
+                                else
+                                    udsMin <= std_logic_vector(to_unsigned(TO_INTEGER(unsigned(udsMin)) + 1, udsMin'length)); --Suma 1 min
                                 end if;
                             else
-                                udsMin <= std_logic_vector(to_unsigned(TO_INTEGER(unsigned(udsMin)) + 1, udsMin'length)); --Suma 1 min
+                                decSecs <= std_logic_vector(to_unsigned(TO_INTEGER(unsigned(decSecs)) + 1, decSecs'length)); --Suma "10" secs
                             end if;
                         else
-                            decSecs <= std_logic_vector(to_unsigned(TO_INTEGER(unsigned(decSecs)) + 1, decSecs'length)); --Suma "10" secs
-                        end if;
-                    else
-                        udsSecs <= std_logic_vector(to_unsigned(TO_INTEGER(unsigned(udsSecs)) + 1, udsSecs'length)); --se hace un cast a unsigned int para sumar 1 y se vuelve a pasar a std_vector
-                    end if;   
-                end if;                       
-            when S2 => --Pause
-                null;
-            when others => udsSecs<="0000"; decSecs<="0000"; udsMin<="0000"; decMin<="0000";          
-        end case;
-        digits_0to3<= decMin & udsMin & decSecs & udsSecs;
-        
-        --Propuesta REVISAR blink control
-        blink_ctrl <= (others => '1');
+                            udsSecs <= std_logic_vector(to_unsigned(TO_INTEGER(unsigned(udsSecs)) + 1, udsSecs'length)); --se hace un cast a unsigned int para sumar 1 y se vuelve a pasar a std_vector
+                        end if;   
+                    end if;                       
+                when S2 => --Pause
+                    null;
+                when others => udsSecs<="0000"; decSecs<="0000"; udsMin<="0000"; decMin<="0000";          
+            end case;
+            digits_0to3<= decMin & udsMin & decSecs & udsSecs;
+            
+            --Propuesta REVISAR blink control
+            blink_ctrl <= (others => '1');
+        end if;
     end process;
 
 end Behavioral;
